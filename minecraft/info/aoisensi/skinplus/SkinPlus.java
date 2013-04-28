73ee4771c27eba9f7a17968392c438346808bf2e
aoisensi <aoisensi@aoisensi.info>
2013-04-28 18:32:24 +0900

    add example.xml and more

--- minecraft/info/aoisensi/skinplus/SkinPlus.java
+++ minecraft/info/aoisensi/skinplus/SkinPlus.java
@@ -1,18 +1,22 @@
 package info.aoisensi.skinplus;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
+import info.aoisensi.skinplus.client.model.ModelPlusBiped;
 import info.aoisensi.skinplus.client.renderer.entity.RenderPlusPlayer;
 import net.minecraft.client.gui.GuiOptions;
 import net.minecraft.client.model.ModelCow;
 import net.minecraft.client.renderer.entity.RenderCow;
+import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.entity.player.EntityPlayer;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
@@ -25,10 +29,28 @@ public class SkinPlus {
 	@Instance("SkinPlus")
     public static SkinPlus instance;
 	
+	public static Field fieldMainModel, fieldModelBipedMain;
+	
+	public Map<String, ModelPlusBiped> RenderPlusMap = new HashMap<String, ModelPlusBiped>();
+	
+	
+	@PreInit
+	public void preLoad(FMLInitializationEvent event) {
+		try {
+			fieldMainModel = RenderLiving.class.getDeclaredField("mainModel");
+			fieldMainModel.setAccessible(true);
+			fieldModelBipedMain = RenderPlayer.class.getDeclaredField("mainModel");
+			fieldModelBipedMain.setAccessible(true);
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Init
 	public void load(FMLInitializationEvent event) {
 		RenderingRegistry.registerEntityRenderingHandler(EntityPlayer.class, new RenderPlusPlayer());
 	}
 	
-	public Map<String, RenderPlus> RenderPlusMap = new HashMap<String, RenderPlus>();
 }
--- minecraft/info/aoisensi/skinplus/client/model/ModelPlusBiped.java
+++ minecraft/info/aoisensi/skinplus/client/model/ModelPlusBiped.java
@@ -5,32 +5,49 @@ import org.lwjgl.opengl.GL11;
 import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.MathHelper;
 
 public class ModelPlusBiped extends ModelBiped {
-	
-    public ModelRenderer plusTest;
-	
-    public ModelPlusBiped()
-    {
-        this(0.0F);
-    }
 
-    public ModelPlusBiped(float par1)
-    {
-        this(par1, 0.0F, 64, 32);
-    }
+	public ModelRenderer plusTest;
 
-    public ModelPlusBiped(float par1, float par2, int par3, int par4)
-    {
-    	super(par1, par2, par3, par4);
-        this.plusTest = new ModelRenderer(this, 0, 0);
-        this.plusTest.addBox(-8.0F, -16.0F, -8.0F, 8, 8, 8, par1);
-        this.plusTest.setRotationPoint(0.0F, 0.0F + par2, 0.0F);
-    }
-    
-    public void render(Entity par1Entity, float par2, float par3, float par4, float par5, float par6, float par7)
-    {
-    	super.render(par1Entity, par2, par3, par4, par5, par6, par7);
-    	this.plusTest.render(par7);
-    }
+	public ModelPlusBiped()
+	{
+		this(0.0F);
+	}
+
+	public ModelPlusBiped(float par1)
+	{
+		this(par1, 0.0F, 64, 32);
+	}
+
+	public ModelPlusBiped(float par1, float par2, int par3, int par4)
+	{
+		super(par1, par2, par3, par4);
+
+		//texture offset
+		this.plusTest = new ModelRenderer(this, 0, 0);
+		//origin size
+		this.plusTest.addBox(-1.0F, -8.0F, -1.0F, 2, 30, 2, par1);
+		//roll
+		this.plusTest.setRotationPoint(5.0F, 0.0F + par2, 0.0F);
+
+		this.bipedHead.addChild(this.plusTest);
+	}
+
+	public void render(Entity par1Entity, float par2, float par3, float par4, float par5, float par6, float par7)
+	{
+		super.render(par1Entity, par2, par3, par4, par5, par6, par7);
+		//this.plusTest.render(par7);
+	}
+
+	float f = 0.0F;
+
+	public void setRotationAngles(float par1, float par2, float par3, float par4, float par5, float par6, Entity par7Entity)
+	{
+		super.setRotationAngles(par1, par2, par3, par4, par5, par6, par7Entity);
+//		plusTest.rotateAngleX = bipedHead.rotateAngleX + f;
+//		plusTest.rotateAngleY = bipedHead.rotateAngleY;
+//		plusTest.rotateAngleZ = bipedHead.rotateAngleZ;
+	}
 }
